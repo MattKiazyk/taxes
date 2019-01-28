007c1230056d13e9d71608f796359daa629d54b9
Matt Kiazyk <matt@bnid.ca>
2019-01-27 23:37:51 -0600

    Updated for 2019 Assessment and 2019 estimated taxes

--- _site/assets/js/ybr.js
+++ _site/assets/js/ybr.js
@@ -1,5 +1,8 @@
 var startYear = 2019
-		
+var startYearAssessment = 0
+var startYearAssessmentClass = ""
+var searchRollNo = 0
+
 //**AUTOCOMPLETE **//
 $(function() {
 	$('#address').autocomplete({
@@ -20,14 +23,14 @@ $(function() {
 						},
 						success: function( data ) {
 							var newData = [];
-							
+
 							// add all the possible address names obtain from server to hint list on client side
 							for (var i = 0; i < data.length; i++) {
 								newData.push({'value':data[i].dROLLNMBR, 'label':data[i].STREET_ADDRESS});
 							} //for each
 							response( newData );
 						},
-					});		
+					});
 	    }
 	});
 });
@@ -35,8 +38,8 @@ $(function() {
 $(document).ready(function () {
 
 	Handlebars.registerHelper('assessmentTable', function(items, options) {
-	
-		var items = options.data.root.taxes;
+
+		var items = options.data.root;
 	  var out = "<table class='responsive'>";
 		out = out + "<tr>"
 		out = out + "<th>Tax Year</th>";
@@ -46,11 +49,18 @@ $(document).ready(function () {
 		out = out + "<th>Buildings</th>";
 		out = out + "<th>Total</th>";
 		out = out + "</tr>"
-		
+
 	  for (year = startYear; year > 2000; year--) {
-			var item = items[year];
+			var index = startYear - year;
+			var item = items[index];
+			if (year == startYear) {
+				startYearAssessment = item['Total']
+				startYearAssessmentClass = item['Class']
+			}
+
 			if (item) {
-				var assessment = item.assessment;
+				var assessment = item;
+
 				if (assessment) {
 					out = out + "<tr>"
 			    out = out + "<td>" + year + "</td>";
@@ -63,6 +73,11 @@ $(document).ready(function () {
 				}
 			}
 	  }
+
+		// We know newer assessment but not in full taxes yet
+		// So call after we have the latest assessment
+		// once City of brandon updates in May - this can be called at the same time
+		loadTaxes()
 	  return out + "</table>";
 	});
 
@@ -73,7 +88,7 @@ $(document).ready(function () {
 		out = out + "<th>Sale Date</th>";
 		out = out + "<th>Consideration</th>";
 		out = out + "</tr>"
-		
+
 	  for (year = startYear; year > 2000; year--) {
 			var item = items[year];
 			if (item) {
@@ -84,82 +99,99 @@ $(document).ready(function () {
 					out = out + "<tr>"
 			    out = out + "<td>" + saleDetail['Sale Date'] + "</td>";
 			    out = out + "<td>" + saleDetail.Consideration + "</td>";
-					out = out + "</tr>"			
+					out = out + "</tr>"
 				}
 			}
 	  }
 	  return out + "</table>";
 	});
-	
+
 	Handlebars.registerHelper('taxesTable', function(items, options) {
 	  var out = "<table class='responsive'>";
 		out = out + "<tr>"
 		out = out + "<th>Type</th>";
-		
-		// add in 2017 estimates
+
+		// add in 2019 estimates
 		var residentalRate = .45
 		var commercialRate = .65
-		
-		var currentAssessment = parseInt(items[2018].assessment['Total'].replace(/,/g,""))
-		var councilIncrease = 0.00940 //0.94% increase
-		var millRate = 15.867
-		var schoolMillRate = 15.048 //14.91 2016 * 0.97
-		console.log(items[2017])
-		console.log(currentAssessment)
 
-		if (items[2017] && currentAssessment) {
+		var currentAssessment = startYearAssessment.replace(/,/g,"")
+		var councilIncrease = 0.01170 //1.17% increase
+		var millRate = 16.034 // 2019 mill rate
+		var schoolMillRate = 15.048 //14.91 2016 * 0.97
+
+		items =  items.reverse(); // sort opposite of what it comes so latest is first
+
+		var item2019 = {}
+
+		var firstItem = items[0]
+		if ((firstItem["TAX_YEAR"].replace(/\s/g,'') === "" + 2018) && currentAssessment) {
 			var rate = residentalRate
-			if (items[2017].assessment['Class'] == "OTHER PROPERTY") {
+			if (startYearAssessmentClass == "OTHER PROPERTY") {
 				rate = commercialRate
 			}
 			var propertyValue = (currentAssessment * rate / 1000 )
-			console.log(propertyValue)
+
 			var municipalBase =  propertyValue * millRate
-			var municipal = municipalBase - items[2017]["Loc_Imp_Debt_Serv"]
-			
-			items[2018]["School_Division"] = propertyValue * schoolMillRate
-			items[2018]["Provincial_Education"] = items[2017]["Provincial_Education"]
-			items[2018]["General_Municipal"] = municipal
-			items[2018]["Loc_Imp_Debt_Serv"] = items[2017]["Loc_Imp_Debt_Serv"]
-			items[2018]["Gross_Tax_Amount"] = items[2018]["General_Municipal"] + items[2018]["Loc_Imp_Debt_Serv"] + items[2018]["School_Division"] + items[2018]["Provincial_Education"] 
-			items[2018]["Home_Owner_Grant"] = items[2017]["Home_Owner_Grant"] 
-			items[2018]["Net_Tax_Amount"] = items[2018]["Gross_Tax_Amount"] - items[2018]["Home_Owner_Grant"] 
+			var municipal = municipalBase - firstItem["LOCAL_IMPROVEMENTS"]
+
+			item2019["BRANDON_SCHOOL_DIVISION"] = Number(firstItem["BRANDON_SCHOOL_DIVISION"]) //propertyValue * schoolMillRate
+			item2019["PROVINCIAL_SCHOOL"] = Number(firstItem["PROVINCIAL_SCHOOL"])
+			item2019["GENERAL_MUNICIPAL"] = Number(municipal)
+			item2019["LOCAL_IMPROVEMENTS"] = Number(firstItem["LOCAL_IMPROVEMENTS"])
+		 	item2019["GROSS_TAX"] = item2019["GENERAL_MUNICIPAL"] + item2019["LOCAL_IMPROVEMENTS"] + item2019["BRANDON_SCHOOL_DIVISION"] + item2019["PROVINCIAL_SCHOOL"]
+			item2019["HOG_AMOUNT_CLAIMED"] = firstItem["HOG_AMOUNT_CLAIMED"]
+			item2019["NET_TAX"] = item2019["GROSS_TAX"] - item2019["HOG_AMOUNT_CLAIMED"]
+			item2019["TAX_YEAR"] = "2019"
+		  items.unshift(item2019)
 		}
-		
-		
-		for (year = startYear - 1; year > 2000; year--) {
-			if (items[year]) {	
+
+
+		for (year = startYear; year > 2000; year--) {
+			var item = null;
+			for (index = 0; index < items.length - 1; index++) {
+				var i = items[index]
+				if (i["TAX_YEAR"].replace(/\s/g,'') === "" + year) {
+					item = i
+					break;
+				}
+
+			}
+			if (item) {
 				out = out + "<th>" + year + "</th>"
+			}
+			else {
+				console.log("Missing year: " + year)
 			}
 		}
 		out = out + "</tr>"
 
 	 	out = out + "<tr>"
-	  out = out + "<td>School Division</td>" + yearLoop(items, "School_Division");
+	  out = out + "<td>School Division</td>" + yearLoop(items, "BRANDON_SCHOOL_DIVISION");
 		out = out + "</tr>"
 		out = out + "<tr>"
-	  out = out + "<td>Provincial Education</td>" + yearLoop(items, "Provincial_Education");
-		out = out + "</tr>"		
+	  out = out + "<td>Provincial Education</td>" + yearLoop(items, "PROVINCIAL_SCHOOL");
+		out = out + "</tr>"
 	  out = out + "<tr>"
-	  out = out + "<td>General Municipal</td>" + yearLoop(items, "General_Municipal");
+	  out = out + "<td>General Municipal</td>" + yearLoop(items, "GENERAL_MUNICIPAL");
 		out = out + "</tr>"
 		out = out + "<tr>"
-	  out = out + "<td>Local Improvements</td>" + yearLoop(items, "Loc_Imp_Debt_Serv");
+	  out = out + "<td>Local Improvements</td>" + yearLoop(items, "LOCAL_IMPROVEMENTS");
 		out = out + "</tr>"
 		out = out + "<tr>"
-	  out = out + "<td>Gross Tax Amount</td>" + yearLoop(items, "Gross_Tax_Amount");
+	  out = out + "<td>Gross Tax Amount</td>" + yearLoop(items, "GROSS_TAX");
 		out = out + "</tr>"
 		out = out + "<tr>"
-	  out = out + "<td>Home Owner Grant</td>" + yearLoop(items, "Home_Owner_Grant");
+	  out = out + "<td>Home Owner Grant</td>" + yearLoop(items, "HOG_AMOUNT_CLAIMED");
 		out = out + "</tr>"
 		out = out + "<tr>"
-	  out = out + "<td>Net Tax Amount</td>" + yearLoop(items, "Net_Tax_Amount");
+	  out = out + "<td>Net Tax Amount</td>" + yearLoop(items, "NET_TAX");
 		out = out + "</tr>"
 		out = out + "</table>"
-		
-	  return out + "<strong>* 2018 ESTIMATED - Based on proposed millRate increases with 2018 property assessments. Actual 2018 property taxes may vary in May</strong> <br/><br/>";
+
+	  return out + "<strong>* 2019 ESTIMATED - Based on proposed millRate increases with ACTUAL 2019 property assessments. Actual 2019 property taxes may vary in May</strong> <br/><br/>";
 	});
-	
+
 	Handlebars.registerHelper('metaTable', function(items, options) {
 		var items = options.data.root.taxes;
 		var out = "<table class='responsive'>"
@@ -179,17 +211,26 @@ $(document).ready(function () {
 			out = out + "</tr>"
 		}
 		out = out + "</table>"
-		
-	
+
+
 		return out
 	})
 });
 
 function yearLoop(years, item) {
 	var out = ""
-	for (year = startYear - 1; year > 2000; year--) {
-		if (years[year]) {
-			out = out + "<td>$" + Number(years[year][item]).formatMoney()  + "</td>"
+	for (year = startYear; year > 2000; year--) {
+		var distinctItem = null;
+		for (index = 0; index < years.length - 1; index++) {
+			var i = years[index]
+			if (i["TAX_YEAR"].replace(/\s/g,'') === "" + year) {
+				distinctItem = i
+				break;
+			}
+		}
+
+		if (distinctItem) {
+			out = out + "<td>$" + Number(distinctItem[item]).formatMoney()  + "</td>"
 		}
 	}
 	return out
@@ -201,70 +242,115 @@ $( window ).load(function() {
 	if (search == null) {
 			return
 		}
-	
+  searchRollNo = search
 	$('html, body').animate({
   	scrollTop: $("#mainData").offset().top
 	});
 
-	//var assessmentHTML = getAssessmentHTML(search)	
-	
-	var assessmentSource = $('#assessment-template').html();
-	var assessmentTemplate = Handlebars.compile(assessmentSource);
-	var metaSource = $('#meta-template').html();
-	var metaTemplate = Handlebars.compile(metaSource);
+	// Loads a map showing the property in satellite view
+	loadMap(search);
+
+	// Load assessments dynamically
+	loadAssessments(search);
+
+	// sales and meta are currently loaded from firebase still
+	loadTaxesFromFirebase();
+
+});
+
+function loadTaxes() {
+	var search = getParameterByName('rollNumber')
+	if (search == null) {
+			return
+		}
+
 	var taxesSource = $('#taxes-template').html();
 	var taxesTemplate = Handlebars.compile(taxesSource);
+
+	$('#taxes-wrapper').wait();
+
+	var url = "http://opendata.brandon.ca/opendataservice/default.aspx?format=json&dataset=tax&columns=ROLL_NUMBER&values=" + search;
+	console.log("Loading property tax information at: " + url);
+
+	$.getJSON( url, function( data ) {
+
+		// if user fills in an invalid rollNumber. Quit
+		if (!data) {
+			$('#taxes-wrapper').unwait();
+			return
+		}
+
+		var taxesJSON = {"taxes":data};
+		var taxesHTML = taxesTemplate(taxesJSON)
+
+		$('#taxes-template').replaceWith(taxesHTML)
+		$('#taxes-wrapper').unwait();
+
+		// To make tables responsive after we've udpated them
+		updateTables();
+	});
+}
+
+function loadTaxesFromFirebase() {
+	var search = getParameterByName('rollNumber')
+	if (search == null) {
+			return
+		}
+
+	var metaSource = $('#meta-template').html();
+	var metaTemplate = Handlebars.compile(metaSource);
+
 	var saleSource = $('#sales-template').html();
 	var saleTemplate = Handlebars.compile(saleSource);
-	
+
 	$('#meta-wrapper').wait();
-	$('#taxes-wrapper').wait();
-	$('#assessment-wrapper').wait();
+
 	$('#sales-wrapper').wait();
-	
-	// Loads a map showing the property in satellite view 
-	loadMap(search);
-	
+
 	// load Taxes from Firebase Databases
 	firebase.initializeApp({
 		databaseURL:'https://ybr.firebaseio.com/'
 	})
-	
+
 	firebase.database().ref('taxes/' + search).on("value", function(snapshot) {
 
-		// if user fills in an invalid rollNumber. Quit
-		if (!snapshot.exists()) {
-			$('#taxes-wrapper').unwait();
-			return
-		}
 		var taxesJSON = {"taxes":snapshot.val()};
-		var taxesHTML = taxesTemplate(taxesJSON)
 
-		var assessmentHtml = assessmentTemplate(taxesJSON)
 		var metaHTML = metaTemplate(taxesJSON)
 		var saleHTML = saleTemplate(taxesJSON)
-			
-		$('#taxes-template').replaceWith(taxesHTML)
-		$('#taxes-wrapper').unwait();
-		
-		$('#assessment-template').replaceWith(assessmentHtml);
-		$('#assessment-wrapper').unwait();	
-		
+
+
 		$('#meta-template').replaceWith(metaHTML);
-		$('#meta-wrapper').unwait();	
-		
+		$('#meta-wrapper').unwait();
+
 		$('#sales-template').replaceWith(saleHTML);
 		$('#sales-wrapper').unwait();
-		
+
 		// To make tables responsive after we've udpated them
 		updateTables();
 
 	}, function (errorObject) {
 	  console.log("The read failed: " + errorObject.code);
 	});
+}
 
+function loadAssessments(rollNo) {
+	$('#assessment-wrapper').wait();
+	var assessmentSource = $('#assessment-template').html();
+	var assessmentTemplate = Handlebars.compile(assessmentSource);
 
-});
+	var url = 'http://www.mattkiazyk.com/bites/005/assess.php?type=json&roll=' + rollNo;
+	console.log("Loading assessments dynamically at: " + url + " via https://web22.gov.mb.ca/MAO/Public/summary.aspx?lang=EN&extrct_prop_id=17572741&roll_id=XXXXXXX&tax_year=2019&extr_typ_ind=T");
+	$.getJSON( url, function( data ) {
+  	var items = [];
+		var assessmentHtml = assessmentTemplate(data.assessment);
+
+		$('#assessment-template').replaceWith(assessmentHtml);
+		$('#assessment-wrapper').unwait();
+
+		updateTables();
+	});
+}
 
 function loadMap(rollNo) {
 	var map = L.map('map', {
@@ -282,7 +368,7 @@ function loadMap(rollNo) {
 	// 	layers : [1,2,4,8,10],
 	// 	imageSR : 26914
 	// }).addTo(map).bringToFront();
-	
+
 	// Shows property polygon
 	var find = L.esri.find({
 		url: 'https://gisapp.brandon.ca/arcgis/rest/services/Open_Data/Open_Data/MapServer'
@@ -292,7 +378,7 @@ function loadMap(rollNo) {
 			.text(rollNo)
 			.fields('ROLL_NUM')
 			.contains(false)
-		
+
 	find.run(function(error, featureCollection, response){
 		var polygon =	L.geoJson(featureCollection, {
 			style: style
@@ -302,16 +388,16 @@ function loadMap(rollNo) {
 		// fit the map to around the polygon
 		map.fitBounds(polygon.getBounds().pad(0.5));
 	});
-	
+
 	// can't find lead pipes on gisapp services
-		// 
+		//
 		// // LEAD PIPES
 		// var query = L.esri.query({
 		// 	    url:'http://gis.brandon.ca/arcgis/rest/services/COBRA/COBRAmapService/MapServer/12'
 		// 	});
-		// 	
+		//
 		// 	query.where("InstallDate<='Jan 1, 1952'");
-		// 	
+		//
 		// 	query.run(function(error, featureCollection, response){
 		// 	    console.log('Found ' + featureCollection.features.length + ' features');
 		// 			var polyLine =	L.geoJson(featureCollection, {
@@ -319,22 +405,22 @@ function loadMap(rollNo) {
 		// 				onEachFeature: onEachFeature
 		// 			});
 		// 			polyLine.addTo(map).bringToFront();
-		// 
+		//
 		// 	});
-		// 	
+		//
 		// 	var info = L.control();
-		// 
+		//
 		// 	info.onAdd = function (map) {
 		// 	    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
 		// 	    this.update();
 		// 	    return this._div;
 		// 	};
-		// 
+		//
 		// 	// method that we will use to update the control based on feature properties passed
 		// 	info.update = function (props) {
 		// 	    this._div.innerHTML = '<h4>Lead Pipes</h4>Click on any red line to<br />show info on potential lead pipes.</br><a href="http://www.brandon.ca/water-treatment/lead-water-services-information">More Info</a>';
 		// 	};
-		// 
+		//
 		// 	info.addTo(map);
 }
 
